d023e94b615c2d970a58a5c825486ab9af53f869
Musab Momin <musab.momin@kapso.in>
2022-09-05 20:50:19 +0530

    detail show page is done, fixed some useEffect issues

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -6,12 +6,40 @@ const Show = () => {
     
     const { id } = useParams();
     const [show, setShow] = useState(null)
-
+    const [loading, setLoading] = useState(true)
+    const [errors, setErrors] = useState(false);
 
     useEffect(()=>{
+        
+        let isMounting = true
+        
+        setLoading(true)
         getApi(`shows/${id}?embed=cast`)
-        .then(data => setShow(data))
-    }, [])
+        .then(data => {
+            if(isMounting){
+                setLoading(false)
+                setShow(data)
+            }
+        })
+        .catch(err=>{
+            if(isMounting){
+                setLoading(false)
+                setErrors(err)
+            }
+        })
+
+        return () => {
+            isMounting = false
+        }
+    }, []);
+
+    if(loading){
+        return( <div>Loading...</div> )
+    }
+
+    if(errors){
+        return( <div>Oops! An error accured</div>)
+    }
   
     return (
     <div>Show</div>
